ee25b7bdbd5a9858cade3574fa11fce7958a6ff8
Vladislav Yagello <y.surprise@gmail.com>
2021-12-23 22:48:42 +0300

    Homework03

---
+++ Homework03/Task01/Account.cs
@@ -0,0 +1,72 @@
+﻿namespace Task01
+{
+    public enum AccountType
+    {
+        Card,
+        Deposit,
+        Loan
+    }
+
+    public class Account
+    {
+        private static int _counter = 0;
+        private int _number;
+        private decimal _balance;
+        private AccountType _accountType;
+
+        public int Number
+        {
+            get => _number;
+            set => _number = value;
+        }
+
+        public decimal Balance
+        {
+            get => _balance;
+            set => _balance = value;
+        }
+
+        public AccountType AccountType
+        {
+            get => _accountType;
+            set => _accountType = value;
+        }
+
+        private static int GetCounter() => _counter++;
+
+        public Account() : this(0, AccountType.Card) { }
+
+        public Account(decimal balance) : this(balance, AccountType.Card) { }
+
+        public Account(AccountType accountType) : this(0, accountType) { }
+
+        public Account(decimal balance, AccountType accountType)
+        {
+            _number = GetCounter();
+            _balance = balance;
+            _accountType = accountType;
+        }
+
+        public void Put(decimal amount) => _balance += amount;
+
+        public bool Withdraw(decimal amount)
+        {
+            if (_balance >= amount)
+            {
+                _balance -= amount;
+                return true;
+            }
+            return false;
+        }
+
+        public bool TransferHere(Account account, decimal amount)
+        {
+            if (account.Withdraw(amount))
+            {
+                Put(amount);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Homework03/Task01/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(Reverse("123"));
+            EmailToFile("../../in.txt", "../../out.txt");
+
+
+            Account account1 = new(1000, AccountType.Deposit);
+            Account account2 = new(2000, AccountType.Deposit);
+            WriteLine($"{account1.Number} {account1.Balance} {account1.AccountType}");
+            WriteLine($"{account2.Number} {account2.Balance} {account2.AccountType}");
+            WriteLine();
+
+            account1.TransferHere(account2, 2500);
+            WriteLine($"{account1.Number} {account1.Balance} {account1.AccountType}");
+            WriteLine($"{account2.Number} {account2.Balance} {account2.AccountType}");
+            WriteLine();
+
+            account1.TransferHere(account2, 500);
+            WriteLine($"{account1.Number} {account1.Balance} {account1.AccountType}");
+            WriteLine($"{account2.Number} {account2.Balance} {account2.AccountType}");
+            WriteLine();
+        }
+
+        static string Reverse(string str)
+        {
+            char[] buf = str.ToCharArray();
+            Array.Reverse(buf);
+            return new String(buf);
+        }
+
+        public static void SearchMail(ref string s)
+        {
+            string[] split = s.Split('&');
+            s = split.Length == 2 ? split[1].Trim() : null;
+        }
+
+        public static void EmailToFile(string sourcePath, string destinationPath)
+        {
+            if (!File.Exists(sourcePath))
+                return;
+
+            StreamReader reader = new(sourcePath);
+            StreamWriter writer = new(destinationPath);
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                SearchMail(ref line);
+                if (line != null)
+                    writer.WriteLine(line);
+                line = reader.ReadLine();
+            }
+            reader.Close();
+            writer.Close();
+        }
+    }
+}
