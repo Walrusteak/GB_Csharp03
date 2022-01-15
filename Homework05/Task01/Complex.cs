d02b66749d17d6f14caeaa263a28d7f4aeef02b8
Vladislav Yagello <y.surprise@gmail.com>
2022-01-15 19:20:42 +0300

    Homework05

---
+++ Homework05/Task01/Complex.cs
@@ -0,0 +1,48 @@
+﻿namespace Task01
+{
+    public struct Complex
+    {
+        private float _real;
+        private float _imaginary;
+
+        public Complex(float real, float imaginary)
+        {
+            _real = real;
+            _imaginary = imaginary;
+        }
+
+        public float Real => _real;
+
+        public float Imaginary => _imaginary;
+
+        public override string ToString() => $"{_real} + {_imaginary}i";
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Complex complex)
+                return this == complex;
+            return false;
+        }
+
+        public override int GetHashCode() => base.GetHashCode() ^ _real.GetHashCode() ^ _imaginary.GetHashCode();
+
+        public static bool operator ==(Complex first, Complex second) => first._real == second._real && first._imaginary == second._imaginary;
+
+        public static bool operator !=(Complex first, Complex second) => !(first == second);
+
+        public static Complex operator +(Complex first, Complex second) => new(first._real + second._real, first._imaginary + second._imaginary);
+
+        public static Complex operator -(Complex first, Complex second) => new(first._real - second._real, first._imaginary - second._imaginary);
+
+        public static Complex operator *(Complex first, Complex second)
+        {
+            return new(first._real * second._real - first._imaginary * second._imaginary, first._imaginary * second._real + first._real * second._imaginary);
+        }
+
+        public static Complex operator /(Complex first, Complex second)
+        {
+            return new((first._real * second._real + first._imaginary * second._imaginary) / (second._real * second._real + second._imaginary * second._imaginary),
+                (first._imaginary * second._real - first._real * second._imaginary) / (second._real * second._real + second._imaginary * second._imaginary));
+        }
+    }
+}
---
+++ Homework05/Task01/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Console;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational r1 = new(1, 2);
+            r1++;
+            WriteLine(r1);
+            r1++;
+            WriteLine(r1);
+            r1--;
+            WriteLine(r1);
+
+            Rational r2 = r1 + new Rational(2, 3);
+            WriteLine(r2);
+            Rational r3 = r1 + new Rational(4, 2);
+            WriteLine(r3);
+
+            int n = new Rational(10, 2) % new Rational(9, 3);
+            WriteLine(n);
+
+            WriteLine(new Rational(1, 2) == new Rational(5, 10));
+            WriteLine();
+
+            Complex c1 = new(1, 2);
+            WriteLine(c1);
+            Complex c2 = new(3, 4);
+            WriteLine(c2);
+            Complex c3 = c1 + c2;
+            WriteLine(c3);
+            Complex c4 = c1 - c2;
+            WriteLine(c4);
+            Complex c5 = c1 * c2;
+            WriteLine(c5);
+            Complex c6 = c1 / c2;
+            WriteLine(c6);
+            WriteLine(new Complex(5, 5) == new Complex(5, 5));
+        }
+    }
+}
