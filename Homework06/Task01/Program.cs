a77963b16877ef6716c0e068798502d958e549b2
Vladislav Yagello <y.surprise@gmail.com>
2022-01-18 21:36:23 +0300

    Homework06

---
+++ Homework06/Task01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new(1000, AccountType.Card);
+            Account account2 = new(1000, AccountType.Card);
+            Console.WriteLine(account1 == account2);
+            Console.WriteLine(account1.Equals(account2));
+            Console.WriteLine(account1.Equals(new object()));
+            account2.Number = account1.Number;
+            Console.WriteLine(account1 == account2);
+            Console.WriteLine(account1.Equals(account2));
+            Console.WriteLine(account1.Equals(new object()));
+        }
+    }
+}
---
+++ Homework06/Task02/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+
+namespace Task02
+{
+    public class Circle: Point
+    {
+        private readonly int _radius;
+
+        public Circle() : this(0, 0) { }
+
+        public Circle(int x, int y) : this(x, y, 1) { }
+        
+        public Circle(int x, int y, int radius) : this(x, y, radius, Color.Empty, false) { }
+
+        public Circle(int x, int y, int radius, Color color) : this(x, y, radius, color, false) { }
+
+        public Circle(int x, int y, int radius, Color color, bool visible) : base(x, y, color, visible)
+        {
+            _radius = radius;
+        }
+
+        public int Radius => _radius;
+
+        public double GetArea() => Math.PI * _radius * _radius;
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Radius = {_radius}");
+        }
+    }
+}
